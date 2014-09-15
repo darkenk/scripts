a06f85d4c607737270411343f9a61b9662c62c60
Dariusz Kluska <darkenk@gmail.com>
2014-09-15 16:17:41 +0200

    Added autopush
    Signed-off-by: Dariusz Kluska <darkenk@gmail.com>

--- linux/bash/android/scripts.sh
+++ linux/bash/android/scripts.sh
@@ -25,10 +25,48 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source "${DIR}/../../../general.sh"
 
-alias ls='ls --color=always'
+# dsetup_android_lunch
+function dsetup_android_lunch {
+    if [ $# == 1 ]; then
+        echo $1 > ${ANDROID_BUILD_TOP}/last_lunch
+    else
+        echo "use dsetup_android_lunch target"
+    fi
+}
 
-# Setup title for terminator
-echo -en "\e]0;${DCUR_WORKSPACE}\a"
+function dandroid_lunch {
+    if [ -e last_lunch ]; then
+        last_lunch=$(cat last_lunch)
+        echo "Launching last lunch ("${last_lunch}", you can change lunch by dsetup_android_lunch new_target"
+        lunch ${last_lunch}
+    else
+        lunch
+    fi
+}
+
+function dautopush {
+    echo "Please connect device"
+    if [ $(adb get-state) = "unknown" ]; then
+        adb wait-for-device
+    fi
+    adb root
+    adb remount
+    rm ${ANDROID_BUILD_TOP}/last_mmm
+    while read line
+    do
+        echo ${line}
+        echo ${line} >> ${ANDROID_BUILD_TOP}/last_mmm
+    done
+    prefix=${OUT#$ANDROID_BUILD_TOP/};
+    for path in "$(cat ${ANDROID_BUILD_TOP}/last_mmm | grep Install)"
+    do
+        if [ -z "${path}" ]; then
+            continue
+        fi
+        path=${path#"Install: "}
+        dev_path=${path#$prefix}
+        echo Pushing ${ANDROID_BUILD_TOP}/${path} to ${dev_path}
+        adb push ${ANDROID_BUILD_TOP}/${path} ${dev_path}
+    done
+}
