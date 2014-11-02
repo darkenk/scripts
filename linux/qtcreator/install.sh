b175dc93b92e101e1ecb4f10a2f3eca08f811e04
Dariusz Kluska <darkenk@gmail.com>
2014-11-02 21:23:08 +0100

    Add dark theme for qtcreator (zenburn and wombat)
    Signed-off-by: Dariusz Kluska <darkenk@gmail.com>

---
+++ linux/qtcreator/install.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.config/QtProject/qtcreator/styles
+ln -s `pwd`/zenburn.xml ~/.config/QtProject/qtcreator/styles/
+mkdir -p ~/.config/QtProject/qtcreator/stylesheet
+ln -s /home/darkenk/workspace/scripts/linux/qtcreator/wombat-stylesheet.css ~/.config/QtProject/qtcreator/stylesheet/
