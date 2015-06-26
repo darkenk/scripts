bf639165f8899c75382043e480749a62771774de
darkenk <darkenk@gmail.com>
2015-06-26 12:36:05 +0200

    Next small update
    Signed-off-by: darkenk <darkenk@gmail.com>

--- linux/qtcreator/install.sh
+++ linux/qtcreator/install.sh
@@ -2,7 +2,7 @@ CURRENT_DIR=`pwd`
 mkdir -p ~/.config/QtProject/qtcreator/styles
 ln -s ${CURRENT_DIR}/zenburn.xml ~/.config/QtProject/qtcreator/styles/zenburn.xml
 ln -s ${CURRENT_DIR}/solarized-light.xml ~/.config/QtProject/qtcreator/styles/solarized-light.xml
-ln -s ${CURRENT_DIR}/solarized-dark.xml ~/.config/QtProejct/qtcreator/styles/solarized-dark.xml
+ln -s ${CURRENT_DIR}/solarized-dark.xml ~/.config/QtProject/qtcreator/styles/solarized-dark.xml
 mkdir -p ~/.config/QtProject/qtcreator/themes/
 ln -s ${CURRENT_DIR}/solarized-light.creatortheme ~/.config/QtProject/qtcreator/themes/solarized-light.creatortheme
 ln -s ${CURRENT_DIR}/solarized-dark.creatortheme ~/.config/QtProject/qtcreator/themes/solarized-dark.creatortheme
